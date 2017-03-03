b7bbfdb4aac4f120dffc55c55e8313568173817a
Andy Muth <andy@griffonco.com>
2017-03-02 19:19:33 -0800

    adding timezone to carbon jsonizer

--- src/DefaultJsonizers/CarbonJsonizer.php
+++ src/DefaultJsonizers/CarbonJsonizer.php
@@ -3,24 +3,35 @@
 use Carbon\Carbon;
 use Talonon\Jsonizer\JsonizesOutputInterface;
 
-class CarbonJsonizer implements JsonizesOutputInterface  {
+class CarbonJsonizer implements JsonizesOutputInterface {
+
+  private static $_timezone;
+
+  private static function _getTimeZone() {
+    return self::$_timezone ?: (new \DateTimeZone('UTC'));
+  }
+
+  public function SetTimeZone(\DateTimeZone $timeZone) {
+    self::$_timezone = $timeZone;
+  }
+
   /**
    * @param Carbon $date
    * @return string
    */
   public function GetAttributes($date) {
-    return $date->toDateTimeString();
+    return $date->setTimezone(self::_getTimeZone())->toDateTimeString();
   }
 
   public function GetRelationships($date) {
     return [];
   }
 
-  public function GetResourceType() {
-    return 'carbon';
-  }
-
   public function GetRemovedIncludes() {
     return [];
+  }
+
+  public function GetResourceType() {
+    return 'carbon';
   }
 }
